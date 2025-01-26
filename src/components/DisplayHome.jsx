250d74797fabd2407513077d4c4bdb35b7fb1d0f
“sunil <sunilchinthaparthi99@gmail.com>
2025-01-26 02:01:06 -0500

    added sonssdata and album data to mach with the index of the page by getting the id of it using useParams and mapping it to the album data and song data wiith respect to album

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,21 +2,39 @@ import React from "react";
 import NavBar from "./NavBar";
 import { albumsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
+import { songsData } from "../assets/assets";
 
 const DisplayHome = () => {
   return (
     <>
       <NavBar />
-      <div className="my-5 font-bold text 2xl">
-        {albumsData.map((item, index) => (
-          <AlbumItem
-            key={index}
-            image={item.image}
-            name={item.name}
-            desc={item.desc}
-            id={item.id}
-          />
-        ))}
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <divm className="flex overflow-x-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </divm>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl"> todays biggest hits </h1>
+        <divm className="flex overflow-x-auto">
+          {songsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </divm>
       </div>
     </>
   );
